d20a7baa67cf8e604423564cd1890339cdcbc237
AdhithyaSaminathan07 <tsadhithya07@gmail.com>
2026-03-03 10:37:13 +0530

    Demo project

---
+++ Project-Calculator/index.js
@@ -0,0 +1,16 @@
+let input = document.querySelector("input")
+
+function addval(elementVal){
+    input.value += elementVal
+
+}
+function clearVal(){
+    input.value=""
+}
+
+function deleteVal(){
+    input.value = input.value.slice(0,input.value.length-1)
+}
+function evaluateVal(){
+    input.value = eval(input.value)
+}
